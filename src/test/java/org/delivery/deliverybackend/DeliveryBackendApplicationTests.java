3a2f17271f5a746c57ff8a1ed2b6c40f255e84ec
akanshu-09 <akanshuy009@gmail.com>
2026-06-26 15:17:18 +0530

    Initial commit

---
+++ src/test/java/org/delivery/deliverybackend/DeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery.deliverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
